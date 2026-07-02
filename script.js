35ab5f1335e9f3b88e4da9b62612c23c9ea7e4fa
Gem254 <bettv6384@gmail.com>
2026-07-02 13:53:18 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", () => {
+    alert("Deriv login will be connected after your app is registered with Deriv.");
+});
