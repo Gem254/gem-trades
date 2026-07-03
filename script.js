3cebb64acb3078dfdc61e9d72c12292211c89ca0
Gem254 <bettv6384@gmail.com>
2026-07-03 10:20:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const loginBtn = document.getElementById("loginBtn");
 
 loginBtn.addEventListener("click", () => {
     window.location.href =
-        "https://oauth.deriv.com/oauth2/authorize?app_id=33IsXHOsY1092ufzgmaN8";
+        "https://oauth.deriv.com/oauth2/authorize?app_id=33IsXHOsY1092ufzgmaN8";});
 
